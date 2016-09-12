6cf0a499419f7fd5fd2284861f6d991622acdf09
Ramiro Beltran <beltran.ramiro@hotmail.com>
2016-09-12 16:27:40 -0500

    first commit

---
+++ Soothie Baby/NewSoundViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewSoundViewController.swift
+//  Soothie Baby
+//
+//  Created by ramiro beltran on 9/12/16.
+//  Copyright © 2016 Ramiro Beltran. All rights reserved.
+//
+
+import Foundation
